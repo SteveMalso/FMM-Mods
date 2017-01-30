39fc1ab8c4cfcc917c4f60c01da664d14521b630
dany5639 <dany_woozie@hotmail.com>
2017-01-30 23:23:38 +0100

    Added H3 reload animation fix.

---
+++ h3mods/h3weap/reloadAnimations/reloadAnimations_Install.bat
@@ -0,0 +1,7 @@
+color 0A
+
+START /WAIT TTCC.exe -in reloadAnimations.cmdX -out reloadAnimations.cmds -dat ..\..\..\..\..\maps\tags.dat
+
+Type reloadAnimations.cmds | TagTool.exe ..\..\..\..\..\maps\tags.dat
+
+pause
