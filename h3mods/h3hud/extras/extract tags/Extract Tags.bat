e8f59a464fc3382f2e4e93159e6ea33c196da47f
Alex231 <a13x231mcsl@gmail.com>
2017-04-27 21:57:22 +0100

    Halo 3 HUD 2.3
    Added equipment stuff.

--- h3mods/h3hud/extras/extract tags/Extract Tags.bat
+++
@@ -1,4 +0,0 @@
-FOR %%f IN (..\..\scripts\*.cmds) DO (echo extracttag %%~nf tags\%%~nf.tag >> "extractScript.cmds")
-Type "extractScript.cmds")| "h3h_TagTool.exe" "..\..\..\..\..\..\maps/tags.dat"
-del "extractScript.cmds")
-pause
